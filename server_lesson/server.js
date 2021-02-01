3bd50ef948ebec14fdf9c264aad4ce2688ff5674
innobit <innobit@example.com>
2021-02-01 17:52:50 +0530

    second_commit

--- server_lesson/server.js
+++ server_lesson/server.js
@@ -2,6 +2,10 @@ const http = require('http');
 const server= http.createServer((req,res)=>{
     //console.log('request made');
     console.log(req.url,req.method);
+    res.setHeader('Content-Type','text/html');
+    res.write('<p>hello,ninjas</p>');
+    res.write('<p>hello,again</p>');
+    res.end();
      
 });
 server.listen(3000,'localhost',()=>{
