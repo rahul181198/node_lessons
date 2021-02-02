66dfb6f104576fce60756a3b911e0a1be04ae1db
innobit <innobit@example.com>
2021-02-02 11:15:53 +0530

    retrn_html_pages

--- server_lesson/server.js
+++ server_lesson/server.js
@@ -1,11 +1,25 @@
 const http = require('http');
+const fs= require('fs');
 const server= http.createServer((req,res)=>{
     //console.log('request made');
     console.log(req.url,req.method);
     res.setHeader('Content-Type','text/html');
-    res.write('<p>hello,ninjas</p>');
+     
+    //send an html file
+    fs.readFile('./views/index.html',(err,data)=>{
+        if(err)
+        {
+            console.log(err);
+            res.end();
+        }
+        else{
+            res.write(data);
+            res.end();
+        }
+    })
+    /*res.write('<p>hello,ninjas</p>');
     res.write('<p>hello,again</p>');
-    res.end();
+    res.end();*/
      
 });
 server.listen(3000,'localhost',()=>{
