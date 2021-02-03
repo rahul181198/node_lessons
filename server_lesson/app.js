b7b6662dde7c118e99bcc9ea740208c8ddf82e64
innobit <innobit@example.com>
2021-02-03 16:07:38 +0530

    5commit

--- server_lesson/app.js
+++ server_lesson/app.js
@@ -8,18 +8,32 @@ app.set('view engine','ejs');
 app.listen(3000);
 app.get('/',(req,res)=>{
     //res.send('<p>home page</p>');
-    res.sendFile('./views/index.html',{root:__dirname});
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    //res.sendFile('./views/index.html',{root:__dirname});
+     //res.render('index');
+     res.render('index',{title:'home',blogs:blogs});
 })
 app.get('/about',(req,res)=>{
     //res.send('<p>about page</p>');
-    res.sendFile('./views/about.html',{root:__dirname});
+    //res.sendFile('./views/about.html',{root:__dirname});
+    //res.render('about');
+    res.render('about',{title:'about'});
 })
-//redirects
+app.get('/blogs/create',(req,res)=>{
+    //res.render('create');
+    res.render('create',{title:'create a new blog'});
+})
+/*redirects
 app.get('/about-us', (req, res) => {
     res.redirect('/about');
-  });
+  });*/
 
 //404 pages
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
-  });
+    //res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.render('404',{title:'404'});
+});
