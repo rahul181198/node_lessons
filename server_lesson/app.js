a6e45aae8c0daf00521000d7cefbd80673e97f1b
innobit <innobit@example.com>
2021-02-04 10:02:34 +0530

    7commit

--- server_lesson/app.js
+++ server_lesson/app.js
@@ -1,6 +1,7 @@
 const express= require('express');
 const morgan =require('morgan');
 const mongoose = require('mongoose');
+const Blog = require('./models/blog');
 //express app
 const app =express();
 
@@ -33,6 +34,45 @@ app.use((req, res, next) => {
     next();
   });
   app.use(morgan('dev'));
+
+  // mongoose & mongo tests
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+      title: 'new blog',
+      snippet: 'about my new blog',
+      body: 'more about my new blog'
+    })
+  
+    blog.save()
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  app.get('/all-blogs', (req, res) => {
+    Blog.find()
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+  app.get('/single-blog', (req, res) => {
+    Blog.findById('601b7848157a47a24d6e8293')
+      .then(result => {
+        res.send(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  });
+  
+
+
 app.get('/',(req,res)=>{
     //res.send('<p>home page</p>');
     const blogs = [
