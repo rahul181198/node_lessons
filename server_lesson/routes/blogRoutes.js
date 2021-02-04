21697939f93e0ff6698163609f953479efa025a0
innobit <innobit@example.com>
2021-02-04 17:16:53 +0530

    11commit

--- server_lesson/routes/blogRoutes.js
+++ server_lesson/routes/blogRoutes.js
@@ -1,8 +1,16 @@
 const express= require('express');
-const Blog = require('../models/blog');
+//const Blog = require('../models/blog');
+const blogController = require('../controllers/blogController');
 const router= express.Router();
 
 
+router.get('/create', blogController.blog_create_get);
+router.get('/', blogController.blog_index);
+router.post('/', blogController.blog_create_post);
+router.get('/:id', blogController.blog_details);
+router.delete('/:id', blogController.blog_delete);
+module.exports=router;
+/*
 router.get('/',(req,res)=>{
     
     Blog.find().sort({ createdAt: -1 })
@@ -52,6 +60,6 @@ router.delete('/:id', (req, res) => {
       .catch(err => {
         console.log(err);
       });
-  });
+  });*/
   
-module.exports=router;
+  
